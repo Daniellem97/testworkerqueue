fd4dc14654915ff13af0454821100a98f3742b69
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-04-18 12:47:23 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 locals {
-  stacks = [
-    for i in range(1, 1001) : {
-      name           = "${i <= 500 ? "First" : "Second"} Kubernetes Cluster ${i}"
-      description    = "Provisions a Kubernetes cluster"
-      branch         = "master"
-      repository     = "addressbook"
-    }
-  ]
+  stacks = [for i in range(1, 500) : {
+    name        = "Kubernetes Cluster ${i}"
+    description = "Provisions a Kubernetes cluster"
+    branch      = "master"
+    repository  = "addressbook"
+  }]
 }
 
 resource "spacelift_stack" "k8s_cluster" {
@@ -24,5 +22,32 @@ resource "spacelift_run" "this" {
 
   keepers = {
     branch = spacelift_stack.k8s_cluster[each.key].branch
+  }
+}
+
+locals {
+  stacks = [for i in range(1, 500) : {
+    name        = "Other Cluster ${i}"
+    description = "Provisions a Kubernetes cluster"
+    branch      = "master"
+    repository  = "addressbook"
+  }]
+}
+
+resource "spacelift_stack" "different_cluster" {
+  for_each         = { for idx, stack in local.stacks : idx => stack }
+  name             = each.value.name
+  description      = each.value.description
+  branch           = each.value.branch
+  repository       = each.value.repository
+}
+
+resource "spacelift_run" "this" {
+  for_each = { for idx, stack in local.stacks : idx => stack }
+
+  stack_id = spacelift_stack.different_cluster[each.key].id
+
+  keepers = {
+    branch = spacelift_stack.different_cluster[each.key].branch
   }
 }
