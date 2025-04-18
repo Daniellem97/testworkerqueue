519dc2944d6129d26c6a6ef2b1010923def5178c
Daniellem97 <61118080+Daniellem97@users.noreply.github.com>
2025-04-18 13:01:24 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ locals {
     branch      = "master"
     repository  = "addressbook"
   }]
+
+  stacks2 = [for i in range(1, 500) : {
+    name        = "Other Cluster ${i}"
+    description = "Provisions a Kubernetes cluster"
+    branch      = "master"
+    repository  = "addressbook"
+  }]
 }
 
 resource "spacelift_stack" "k8s_cluster" {
@@ -13,6 +20,10 @@ resource "spacelift_stack" "k8s_cluster" {
   description      = each.value.description
   branch           = each.value.branch
   repository       = each.value.repository
+
+  github_enterprise {
+    namespace = "Daniellem97" # Set this to your actual GitHub Enterprise org/user
+  }
 }
 
 resource "spacelift_run" "this" {
@@ -23,31 +34,30 @@ resource "spacelift_run" "this" {
   keepers = {
     branch = spacelift_stack.k8s_cluster[each.key].branch
   }
-}
 
-locals {
-  stacks2 = [for i in range(1, 500) : {
-    name        = "Other Cluster ${i}"
-    description = "Provisions a Kubernetes cluster"
-    branch      = "master"
-    repository  = "addressbook"
-  }]
+  depends_on = [spacelift_stack.k8s_cluster]
 }
 
 resource "spacelift_stack" "different_cluster" {
-  for_each         = { for idx, stack in local.stacks : idx => stack }
+  for_each         = { for idx, stack in local.stacks2 : idx => stack }
   name             = each.value.name
   description      = each.value.description
   branch           = each.value.branch
   repository       = each.value.repository
+
+  github_enterprise {
+    namespace = "Daniellem97"
+  }
 }
 
 resource "spacelift_run" "other" {
-  for_each = { for idx, stack in local.stacks : idx => stack }
+  for_each = { for idx, stack in local.stacks2 : idx => stack }
 
   stack_id = spacelift_stack.different_cluster[each.key].id
 
   keepers = {
     branch = spacelift_stack.different_cluster[each.key].branch
   }
+
+  depends_on = [spacelift_stack.different_cluster]
 }
